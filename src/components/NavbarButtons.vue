668177b73499d43fbc729049620e6fe36020eb89
NamBZ <NamBZ@users.noreply.github.com>
2025-05-02 08:10:40 +0000

    deploy: bede81281d3eb393370a6c88cf4cf5f85d754554

---
+++ src/components/NavbarButtons.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { IconBrandGithub, IconBrandX, IconInfoCircle, IconMoon, IconSun } from '@tabler/icons-vue';
+import { useStyleStore } from '@/stores/style.store';
+
+const styleStore = useStyleStore();
+const { isDarkTheme } = toRefs(styleStore);
+</script>
+
+<template>
+  <c-tooltip :tooltip="$t('home.nav.github')" position="bottom">
+    <c-button
+      circle
+      variant="text"
+      href="https://github.com/CorentinTh/it-tools"
+      target="_blank"
+      rel="noopener noreferrer"
+      :aria-label="$t('home.nav.githubRepository')"
+    >
+      <n-icon size="25" :component="IconBrandGithub" />
+    </c-button>
+  </c-tooltip>
+</template>
+
+<style lang="less" scoped>
+.n-button {
+  &:not(:last-child) {
+    margin-right: 5px;
+  }
+}
+</style>
